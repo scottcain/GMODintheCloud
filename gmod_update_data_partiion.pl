905afeae1c016ca00107ff7a0e9b355601d1c4c6
Ubuntu <ubuntu@ip-10-152-193-218.ec2.internal>
2013-09-26 20:25:46 +0000

    adding more error catching to the data partition update script, cleaned up the UPDATE documentation

--- gmod_update_data_partiion.pl
+++ gmod_update_data_partiion.pl
@@ -38,19 +38,56 @@ sub update0to2_03 {
     my $LOCAL_TOMCAT_BIN       = $LOCAL_FILES . 'usr/share/tomcat7/bin/';
     my $DATA_TOMCAT_BIN        ='/data/usr/share/tomcat7/bin/';
 
-    print STDERR "Making $DATA_WEBAPOLLO_CONFIG\n";
-    make_path($DATA_WEBAPOLLO_CONFIG) or die $!;
-    print STDERR "Copying $LOCAL_WEBAPOLLO_CONFIG" . "config.xml\n";
-    copy     ($LOCAL_WEBAPOLLO_CONFIG . 'config.xml',
-              $DATA_WEBAPOLLO_CONFIG) or die $!;
-    print STDERR "Copying $LOCAL_WEBAPOLLO_CONFIG" . "canned_comments.xml\n";
-    copy     ($LOCAL_WEBAPOLLO_CONFIG . 'canned_comments.xml',
-              $DATA_WEBAPOLLO_CONFIG) or die $!;
+    unless (-d $DATA_WEBAPOLLO_CONFIG) {
+        print STDERR "Making $DATA_WEBAPOLLO_CONFIG\n";
+        make_path($DATA_WEBAPOLLO_CONFIG) or die $!;
+    }
 
-    print STDERR "Making $DATA_TOMCAT_BIN\n"; 
-    make_path($DATA_TOMCAT_BIN) or die $!;
-    print STDERR "Copying $LOCAL_TOMCAT_BIN" . "setenv.sh\n";
-    copy     ($LOCAL_TOMCAT_BIN . 'setenv.sh',
-              $DATA_TOMCAT_BIN) or die $!; 
+    unless (-f $DATA_WEBAPOLLO_CONFIG . 'config.xml') {
+        print STDERR "Copying $LOCAL_WEBAPOLLO_CONFIG" . "config.xml\n";
+        copy     ($LOCAL_WEBAPOLLO_CONFIG . 'config.xml',
+                  $DATA_WEBAPOLLO_CONFIG) or die $!;
+    }
+
+    unless (-f $DATA_WEBAPOLLO_CONFIG . 'canned_comments.xml') { 
+        print STDERR "Copying $LOCAL_WEBAPOLLO_CONFIG" . "canned_comments.xml\n";
+        copy     ($LOCAL_WEBAPOLLO_CONFIG . 'canned_comments.xml',
+                  $DATA_WEBAPOLLO_CONFIG) or die $!;
+    }
+
+    unless (-d $DATA_TOMCAT_BIN) {
+        print STDERR "Making $DATA_TOMCAT_BIN\n"; 
+        make_path($DATA_TOMCAT_BIN) or die $!;
+    }
+
+    unless (-f $DATA_TOMCAT_BIN . 'setenv.sh' ) {
+        print STDERR "Copying $LOCAL_TOMCAT_BIN" . "setenv.sh\n";
+        copy     ($LOCAL_TOMCAT_BIN . 'setenv.sh',
+                  $DATA_TOMCAT_BIN) or die $!; 
+    }
+
+    print STDERR "Adding WebApollo as a JBrowse plugin\n\n";
+    chdir("/var/lib/tomcat7/webapps/WebApollo/jbrowse") or die $!;
+    system("perl bin/add-webapollo-plugin.pl -i data/trackList.json");
+    system("/etc/init.d/tomcat7 restart");
+
+
+    print STDERR <<END
+
+IMPORTANT: This version of GMOD in the Cloud moved the these files to the
+/data partition:  
+
+    /var/lib/tomcat7/webapps/WebApollo/config/config.xml
+    /var/lib/tomcat7/webapps/WebApollo/config/canned_comments.xml
+    /usr/share/tomcat7/bin/setenv.sh
+
+If you modified any of these files in your previous instance of GMOD in
+the Cloud, please obtain those files from the old instance and carefully
+merge them into their counterparts on the /data partition.  DO NOT just
+replace them, as there are new configuration items particularly in the
+WebApollo config.xml file.
+
+END
+;
 }
 
